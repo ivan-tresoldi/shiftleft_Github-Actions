253a810edc09259dfbf7c1b153a45a51ea8a1640
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2021-09-06 14:56:22 +0200

    Add sudo required to perform sandbox scan

--- files/sandboxscan.sh
+++ files/sandboxscan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./twistcli sandbox --u $TL_USER --p $TL_PASS --address https://$TL_CONSOLE --output-file "sandbox.json" --analysis-duration 1m itresoldi/evilpetclinic:latest
+sudo ./twistcli sandbox -u $TL_USER -p $TL_PASS --address https://$TL_CONSOLE --output-file "sandbox.json" --analysis-duration 1m itresoldi/evilpetclinic:latest
 
 if $? == 0 then
    echo "Image Vulnerability scan passed!"
