652cec6a661539fe8056621b48480979742e8036
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2021-09-06 16:29:55 +0200

    Update sandboxscan.sh

--- files/sandboxscan.sh
+++ files/sandboxscan.sh
@@ -2,15 +2,16 @@
 
 sudo ./twistcli sandbox -u $TL_USER -p $TL_PASS --address https://$TL_CONSOLE --output-file "sandbox.json" --analysis-duration 1m itresoldi/evilpetclinic:latest
 
-status = $?
+status=$?
 echo "Exit status is:"
 echo $status
 
 
-if [ "$status" == 0 ] || [ "$BYPASS_SANDBOX" == 0 ]; then
+if [ "$status" == 0 ] || [ $BYPASS_SANDBOX == 1 ]; then
    echo "Image Vulnerability scan passed!"
    exit 0
 else
    echo "Image Vulnerability scan failed!"
    exit 1
 fi
+
