fc53f999a1c0bfc266e5c5442db3eb9a00be6dbd
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2021-09-08 11:56:41 +0200

    Sandbox test with 20m scan

--- files/sandboxscan.sh
+++ files/sandboxscan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo ./twistcli sandbox -u $TL_USER -p $TL_PASS --address https://$TL_CONSOLE --output-file "sandbox.json" --analysis-duration 1m itresoldi/evilpetclinic:latest
+sudo ./twistcli sandbox -u $TL_USER -p $TL_PASS --address https://$TL_CONSOLE --output-file "sandbox.json" --analysis-duration 20m itresoldi/evilpetclinic:latest
 
 status=$?
 echo "Exit status is:"
